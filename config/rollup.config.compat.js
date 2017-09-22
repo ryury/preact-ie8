24249220128f27ee38a256f4a5de4bca06a4f08a
zhengquanbin <zhengquanbin@corp.netease.com>
2017-09-22 20:12:19 +0800

    compat

---
+++ config/rollup.config.compat.js
@@ -0,0 +1,56 @@
+import fs from 'fs';
+import memory from 'rollup-plugin-memory';
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+let pkg = JSON.parse(fs.readFileSync('./package.json'));
+
+let external = Object.keys(pkg.peerDependencies || {}).concat(Object.keys(pkg.dependencies || {}));
+
+let format = process.env.FORMAT==='es' ? 'es' : 'umd';
+
+export default {
+	entry: 'src/compat/index.js',
+	sourceMap: true,
+	moduleName: pkg.amdName,
+	exports: format==='es' ? null : 'default',
+	dest: format==='es' ? pkg.module : pkg.main,
+	format,
+	external,
+	useStrict: false,
+	globals: {
+		'preact': 'preact',
+		'prop-types': 'PropTypes'
+	},
+	plugins: [
+		format==='umd' && memory({
+			path: 'src/compat/index.js',
+			contents: "export { default } from './index';"
+		}),
+		babel({
+			sourceMap: true,
+			exclude: 'node_modules/**',
+			babelrc: false,
+			presets: [
+				['env', {
+					modules: false,
+					loose: true,
+					exclude: ['transform-es2015-typeof-symbol'],
+					targets: {
+						browsers: ['last 2 versions', 'IE >= 9']
+					}
+				}]
+			]
+		}),
+		nodeResolve({
+			jsnext: true,
+			main: true,
+			skip: external
+		}),
+		commonjs({
+			include: 'node_modules/**',
+			exclude: '**/*.css'
+		})
+	].filter(Boolean)
+};
